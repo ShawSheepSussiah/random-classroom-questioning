e52676ee2b42fd6ce73809e85f6030f26f0602e5
Administrator <Administrator@HLAI>
2023-09-30 12:42:58 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        RandomSelectButton = New Button()
+        UniversalButton = New Button()
+        EnglishExclusiveButton = New Button()
+        SelectedStudentLabel = New Label()
+        SuspendLayout()
+        ' 
+        ' RandomSelectButton
+        ' 
+        RandomSelectButton.Location = New Point(80, 512)
+        RandomSelectButton.Name = "RandomSelectButton"
+        RandomSelectButton.Size = New Size(143, 37)
+        RandomSelectButton.TabIndex = 1
+        RandomSelectButton.Text = "随机抽取"
+        RandomSelectButton.UseVisualStyleBackColor = True
+        ' 
+        ' UniversalButton
+        ' 
+        UniversalButton.Location = New Point(277, 512)
+        UniversalButton.Name = "UniversalButton"
+        UniversalButton.Size = New Size(165, 37)
+        UniversalButton.TabIndex = 2
+        UniversalButton.Text = "其他通用"
+        UniversalButton.UseVisualStyleBackColor = True
+        ' 
+        ' EnglishExclusiveButton
+        ' 
+        EnglishExclusiveButton.Location = New Point(482, 512)
+        EnglishExclusiveButton.Name = "EnglishExclusiveButton"
+        EnglishExclusiveButton.Size = New Size(190, 37)
+        EnglishExclusiveButton.TabIndex = 3
+        EnglishExclusiveButton.Text = "英语专属"
+        EnglishExclusiveButton.UseVisualStyleBackColor = True
+        ' 
+        ' SelectedStudentLabel
+        ' 
+        SelectedStudentLabel.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        SelectedStudentLabel.Font = New Font("Microsoft JhengHei UI", 48F, FontStyle.Bold Or FontStyle.Underline, GraphicsUnit.Point)
+        SelectedStudentLabel.Location = New Point(201, 194)
+        SelectedStudentLabel.Name = "SelectedStudentLabel"
+        SelectedStudentLabel.Size = New Size(409, 134)
+        SelectedStudentLabel.TabIndex = 4
+        SelectedStudentLabel.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(784, 561)
+        Controls.Add(SelectedStudentLabel)
+        Controls.Add(EnglishExclusiveButton)
+        Controls.Add(UniversalButton)
+        Controls.Add(RandomSelectButton)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents RandomSelectButton As Button
+    Friend WithEvents UniversalButton As Button
+    Friend WithEvents EnglishExclusiveButton As Button
+    Friend WithEvents SelectedStudentLabel As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    ' 在 Form1.vb 中声明名为 nameList 的列表，用于存储全班名单
+    Private nameList As List(Of String) = New List(Of String) From {
+        "艾梦豪", "陈怡静", "代佳宁", "翟栋轩", "丁嘉祥", "董家乐", "杜宾杰", "段智慧",
+        "冯茜茜", "高瑞康", "耿诗鉴", "耿一涵", "关艺洁", "郭起见", "郭子聪", "郭祖康",
+        "胡世康", "黄茂洁", "李家倩", "李金", "李苏航", "李兴业", "李政航", "梁晨",
+        "马楷杰", "马语嫣", "齐浩添", "任可心", "商钦博", "宋梓鑫", "孙梦欣", "陶政旗",
+        "王靖雯", "王天佑", "王一菡", "王怡斐", "王奕锦", "王雨欣", "魏启武", "武博林",
+        "肖光亚", "谢诗艺", "熊铜辉", "徐靖皓", "徐鲁豫", "薛依蕊", "杨奋瑞", "杨孟哲",
+        "于家豪", "于润", "张聪", "张嘉豪", "张进", "张世维", "张硕彦", "张天祥", "张迎吉",
+        "赵嘉航", "赵雅君", "赵炎晴", "郑博文", "周锦均", "周帅杰", "朱盈澳"
+    }
+
+    ' 在 Form1.vb 中声明名为 japaneseNameList 的列表，用于存储日语名单
+    Private japaneseNameList As List(Of String) = New List(Of String) From {
+        "张嘉豪", "赵炎晴", "梁晨", "熊铜辉", "郭子聪", "李苏航", "耿诗鉴", "武博林"
+    }
+
+    ' 布尔变量，用于跟踪是否排除日语名单
+
+    Private isJapaneseExclusive As Boolean = False
+
+    Private Sub RandomSelectButton_Click(sender As Object, e As EventArgs) Handles RandomSelectButton.Click
+        If nameList.Count > 0 Then
+            Dim random As New Random()
+            Dim selectedIndex As Integer
+
+            If isJapaneseExclusive Then
+                Dim nonJapaneseStudents As New List(Of String)(nameList)
+                For Each student As String In japaneseNameList
+                    If nonJapaneseStudents.Contains(student) Then
+                        nonJapaneseStudents.Remove(student)
+                    End If
+                Next
+
+                If nonJapaneseStudents.Count > 0 Then
+                    selectedIndex = random.Next(0, nonJapaneseStudents.Count)
+                    Dim selectedStudent As String = nonJapaneseStudents(selectedIndex)
+                    SelectedStudentLabel.Text = selectedStudent
+                Else
+                    SelectedStudentLabel.Text = "没有非日语学生可供选择"
+                End If
+            Else
+                selectedIndex = random.Next(0, nameList.Count)
+                Dim selectedStudent As String = nameList(selectedIndex)
+                SelectedStudentLabel.Text = selectedStudent
+            End If
+        Else
+            SelectedStudentLabel.Text = "没有学生可供选择"
+        End If
+    End Sub
+
+    Private Sub EnglishExclusiveButton_Click(sender As Object, e As EventArgs) Handles EnglishExclusiveButton.Click
+        ' 切换到英语专属模式
+        isJapaneseExclusive = Not isJapaneseExclusive
+
+        ' 更新按钮文本
+        UpdateButtonLabels()
+    End Sub
+
+    Private Sub UniversalButton_Click(sender As Object, e As EventArgs)
+        ' 切换回通用模式
+        isJapaneseExclusive = False
+
+        ' 更新按钮文本
+        UpdateButtonLabels()
+    End Sub
+
+    Private Sub UpdateButtonLabels()
+        If isJapaneseExclusive Then
+            EnglishExclusiveButton.Text = "英语专属 (已启用)"
+            UniversalButton.Text = "通用"
+        Else
+            EnglishExclusiveButton.Text = "英语专属"
+            UniversalButton.Text = "通用 (已启用)"
+        End If
+    End Sub
+End Class
